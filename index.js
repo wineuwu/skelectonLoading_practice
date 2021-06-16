7db7ad0d930d6f2227f465099e81eb0da7f9ee57
winnie <asgh03226@gmail.com>
2021-06-16 16:55:43 +0800

    取得 pxexl api 資訊

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const fetchData = async() => {
-  const card = document.querySelector('.item');
-  const title = card.querySelector('h2');
-  const txt = card.querySelector('p');
+const requestUrl = `https://api.pexels.com/v1/photos/2014422`;
 
+document.addEventListener('DOMContentLoaded',async() => {
+  const data = await fetchData(requestUrl);
+  console.log(data);
+  const { url, src,photographer } = data
+  
+  const { large } = src
+
+
+  
+})
+
+
+const fetchData = async(requestUrl) => {
+  const reponse =await fetch(requestUrl,{
+    method: 'GET',
+    headers: {
+        //這邊需要驗證，所以要加上這行，沒有驗證的朋友就不用加了
+        'Authorization': `${config.PEXEL_API_TOKEN}`
+      },
+    }).then(res => res.json())
+    
+  return reponse ;
 }
