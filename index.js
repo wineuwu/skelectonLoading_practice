cfaf8851340af25e2e4467d5031ef5dd2e0d3bbf
winnie <asgh03226@gmail.com>
2021-06-17 11:06:06 +0800

    待解：call api code 429 ，先以假資料帶替

--- index.js
+++ index.js
@@ -2,12 +2,49 @@ const requestUrl = `https://api.pexels.com/v1/photos/2014422`;
 
 document.addEventListener('DOMContentLoaded',async() => {
   const data = await fetchData(requestUrl);
-  console.log(data);
- // insertContent();
+  const {res, status} = data;
+  if(status === 200){
+    insertContent(res,status)
+  }
   
+  
+  setTimeout(fakeDataLoading(),5000)
 })
+const img = document.querySelector('.item__pic');
+const title = document.querySelector(' .item_title ');
+const author = document.querySelector(' .item_author ');
+const txt = document.querySelector(' .item_txt ');
+const content = [
+  img,
+  title,
+  author,
+  txt
+]
+
+const insertContent = (res,status) => {
+    const { photographer, src } = res;
+    const { large } =src;
+
+    content.map(item =>{item.classList.remove('isLoading');})
+    img.innerHTML =`<img src='${large}' alt="我是圖片" />`
+ //   img.innerHTML =`<img src="https://fakeimg.pl/1000x1000">`
+    author.innerHTML =`${photographer}`
+    //author.innerHTML =`i am  photographer`
+    title.innerHTML ='I m title'
+    txt.innerHTML ='Lorem ipsum dolor sit amet,consectetur adipisicing elit. Vitae nobisLorem ipsum dolor sit amet,consectetur adipisicing elit. Vitae nobis'
+
+}
 
 
+const fakeDataLoading = () => {
+  content.map(item =>{item.classList.remove('isLoading');})
+ 
+  img.innerHTML =`<img src="https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?cs=srgb&dl=pexels-kaique-rocha-598917.jpg&fm=jpg">`
+  author.innerHTML ='Kaique Rocha'
+  title.innerHTML ='Camera'
+  txt.innerHTML ='Lorem ipsum dolor sit amet,consectetur adipisicing elit. Vitae nobisLorem ipsum dolor sit amet,consectetur adipisicing elit. Vitae nobis'
+
+}
 
 const fetchData = async(requestUrl) => {
   const response = await fetch(requestUrl,{
@@ -15,7 +52,9 @@ const fetchData = async(requestUrl) => {
     headers: {
         'Authorization': `${config.PEXEL_API_TOKEN}`
       },
-    }).then(res => res.json())
+    })
 
-    
+    const status = response.ok;  
+    const res = await response.json();
+    return { res , status };    
 }
